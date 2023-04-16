3589dadbeaae9081104f2bfb2c6b502c36fde5c8
noverlean <noverlin1352@gmail.com>
2023-04-16 21:35:34 +0300

    ..

--- translator.js
+++ translator.js
@@ -1,24 +1,60 @@
 window.onload = TranslatePage();
 
-var languageState = true;
+var languageState = false;
 
 function TranslatePage()
 {
+    let loadLanguageArray;
     var russian = 
     [
-        "русский",
-        "русский1",
-        "русский2",
+        "Товары",
+        "Отзывы",
+        "О нас",
+        "En",
+        "Добро пожаловать в наш магазин шпаг для комаров, где вы можете найти уникальный подарок для вашего маленького друга! Мы рады представить вам лучшие шпашки по лучшей цене",
+        "Наш слоган",
+        "«И комар лошадь свалит, но элегантнее на шпагах»",
+        "Множество положительных отзывов!!! - наше кредо.",
+        "Комар Павел",
+        "Никогда не видел ничего лучше. 5 из 5!!!",
+        "Комар Екатерина",
+        "Лучше пудры в моем клеше может быть только двуствольный дробовик и классическая шпага.",
+        "Комар Вениамин",
+        "А я просто шизанутый! Ы!0_0",
+        "Комар Суслик02",
+        "Круто, очень кравится, советую.",
+        "Комар Артемий",
+        "Я хоть и панк, но мне не ***. Очень крутой продукт, советую!",
+        "Комар Петр",
+        "Ле-ле-лет ми дай... Ах да, очень нравится, 5/5...",
+        "Комар Елизавета",
+        "Ты котик - я коти... Комарик... Бззз... А так советую конечно.",
     ];
     var english = 
     [
-        "english",
-        "english1",
-        "english2",
+        "Products",
+        "Reviews",
+        "About us",
+        "Ru",
+        "Welcome to our mosquito sword store where you can find a unique gift for your little friend! We are pleased to present you the best skewers at the best price.",
+        "Our slogan",
+        "“And a mosquito will knock down a horse, but it’s more elegant on swords”",
+        "Lots of positive feedback!!! is our creed.",
+        "Komar Pavel",
+        "Never seen anything better. 5 out of 5!!!",
+        "Komar Ekaterina",
+        "Better powder in my flare can only be a double-barreled shotgun and a classic sword.",
+        "Mosquito Benjamin",
+        "And I'm just crazy! S!0_0",
+        "Mosquito Suslik02",
+        "Cool, very cool, I advise.",
+        "Mosquito Artemy",
+        "Even though I'm a punk, I don't ***. Very cool product, I recommend!",
+        "Komar Petr",
+        "Le-le-let me die... Oh yeah, love it, 5/5...",
+        "Mosquito Elizabeth",
+        "You are a cat - I am a cat ... Mosquito ... Bzzz ... And of course I advise you.",
     ];
-
-    console.log(document.cookie);
-    let loadLanguageArray;
 
     if (languageState)
         loadLanguageArray = english;
@@ -26,8 +62,12 @@ function TranslatePage()
         loadLanguageArray = russian;    
 
     loadLanguageArray.forEach(function(item, index) {
-        document.getElementById(index).innerHTML = item;
-        });
+        try
+        {
+            document.getElementById(index).innerHTML = item;
+        }      
+        catch{}  
+    });
 }
 
 function ChangeLanguage()
